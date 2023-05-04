eac44478b203179c8f9ce6f4d8e12e138559f53a
pengbo <34164972@qq.com>
2023-05-04 18:16:53 +0800

    前端编译后dist提交

---
+++ frontend/dist/assets/system-ad3083f0.js
@@ -0,0 +1,2 @@
+import{n as t,A as e}from"./index-66da63bc.js";function r(){return t.get(e.SystemInfo)}function i(){return t.get(e.SystemRequestStatistics)}function n(s){return t.post(e.ServerLogs,s)}function g(s){return t.post(e.ProxyLogs,s)}export{i as a,n as b,g as c,r as g};
+//# sourceMappingURL=system-ad3083f0.js.map
