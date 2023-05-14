79c558fbf797121dd97eab98e2276499d24d2eae
oracle8i <xjg@ncepu.edu.cn>
2023-05-14 12:03:39 +0800

    修订vite编译配置文件

--- frontend/vite.config.dev.ts
+++ frontend/vite.config.dev.ts
@@ -7,7 +7,7 @@ import Components from "unplugin-vue-components/vite";
 import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 import { transformerDirectives } from "unocss";
 import { fileURLToPath, URL } from "node:url";
-
+import fs from "fs"
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
@@ -27,10 +27,33 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("../src", import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   define: {
     "import.meta.env.PACKAGE_VERSION": JSON.stringify(process.env.npm_package_version),
   },
+  mode: "development",
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    fs: {
+      strict: true,
+    },
+    open: true,
+    https: {
+      // 主要是下面两行的配置文件，不要忘记引入 fs 和 path 两个对象
+      cert: fs.readFileSync(fileURLToPath(new URL("src/ssl/cert.crt", import.meta.url))),
+      key: fs.readFileSync(fileURLToPath(new URL("src/ssl/cert.key", import.meta.url)))
+    },
+    proxy: {
+      "/api": {
+        // target: "http://127.0.0.1:8000",        
+        target: "http://ai.quwancode.com/api",
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  }
 });
